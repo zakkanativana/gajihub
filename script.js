46a6713824540fab1d264501de7e1f5214cddd96
zakkanativana <zakkanativana13@gmail.com>
2022-12-24 22:20:30 +0700

    first commit siuuu

---
+++ script.js
@@ -0,0 +1,37 @@
+// Navbar
+$(function () {
+    $(document).scroll(function (){
+        var $nav = $(".navbar-fixed-top, .nav-link");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
+
+// Carousel
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+});
